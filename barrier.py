d65ca355a557f336829946ed2f76ed64399495e9
Giovane Liberato <giovaneliberato@gmail.com>
2019-05-28 20:13:10 -0300

    Improves scenario

--- barrier.py
+++ barrier.py
@@ -1,34 +1,40 @@
 #coding: utf-8
 
 import threading
+import time
 
 count = 0
 
 def thread(char, n, mutex, barrier):
     with mutex:
-        print("RENDEZVOUS %s" % char)
-
+        print("\tRENDEZVOUS %s" % char)
 
     barrier.release()
-
-    while barrier._value < n:
-        pass
+    while barrier._value < n: pass
 
     global count
     count += 1
-    print("CHAR %s - COUNT + 1 = %d" % (char, count))
+    print("\tCHAR %s - COUNT + 1 = %d" % (char, count))
 
 def main():
-    n = 3
+    chars = "ABCDEFGHIJ"
+    n = len(chars)
     barrier = threading.Semaphore(0)
     mutex = threading.Semaphore(n)
 
-    t_A = threading.Thread(target=thread, args=('A', n, mutex, barrier))
-    t_B = threading.Thread(target=thread, args=('B', n, mutex, barrier))
-    t_C = threading.Thread(target=thread, args=('C', n, mutex, barrier))
-    t_A.start()
-    t_B.start()
-    t_C.start()
+    context_thread = lambda ch: threading.Thread(target=thread, args=(ch, n, mutex, barrier))
+
+    threads = []
+    for ch in chars:
+        threads.append(context_thread(ch))
+
+    print("---- STARTING %d THREADS" % n)
+    [t.start() for t in threads]
+
+    while count < n: pass
+    time.sleep(1)
+    print("---- FINAL VALUE OF COUNT %d" % count)
+
 
 if __name__ == '__main__':
     main()
